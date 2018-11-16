15cfd4eaab9f074837a4b679cdbd57c050cb7f90
MichaelPerkins2 <michael.perkins.d@gmail.com>
2018-11-16 16:14:40 -0700

    added user forms

---
+++ src/components/projects/ProjectDetails.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+const ProjectDetails = (props) => {
+    const id = props.match.params.id;
+    return (
+        <div className="container section project-details">
+            <div className="card z-depth-0">
+                <div className="card-content">
+                    <span className="card-title">Project Title - {id}</span>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, incidunt at necessitatibus temporibus ducimus quidem ab excepturi, quae quibusdam quaerat dignissimos cum sequi eveniet officiis culpa unde tenetur, animi velit?</p>
+                </div>
+                <div className="card-action grey lighten-4 grey-text">
+                    <div>Posted by: Mike</div>
+                    <div>16th of November, 1pm</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ProjectDetails
